7943d7ce017483746263473921ded68f341febb3
PoKai, Hsieh <42527125+Hpokai@users.noreply.github.com>
2018-08-20 16:38:15 +0800

    Add program to project

---
+++ GetDocumentsViaFTP/GetDocumentsViaFTP/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace GetDocumentsViaFTP
+{
+    partial class ftp_tester
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ip_textBox = new System.Windows.Forms.TextBox();
+            this.id_textBox = new System.Windows.Forms.TextBox();
+            this.pw_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.logs_listBox = new System.Windows.Forms.ListBox();
+            this.download_timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ip_textBox
+            // 
+            this.ip_textBox.Location = new System.Drawing.Point(47, 12);
+            this.ip_textBox.Name = "ip_textBox";
+            this.ip_textBox.Size = new System.Drawing.Size(290, 20);
+            this.ip_textBox.TabIndex = 0;
+            this.ip_textBox.Text = "192.168.0.55";
+            // 
+            // id_textBox
+            // 
+            this.id_textBox.Location = new System.Drawing.Point(47, 38);
+            this.id_textBox.Name = "id_textBox";
+            this.id_textBox.Size = new System.Drawing.Size(290, 20);
+            this.id_textBox.TabIndex = 0;
+            this.id_textBox.Text = "dgm";
+            // 
+            // pw_textBox
+            // 
+            this.pw_textBox.Location = new System.Drawing.Point(47, 64);
+            this.pw_textBox.Name = "pw_textBox";
+            this.pw_textBox.Size = new System.Drawing.Size(290, 20);
+            this.pw_textBox.TabIndex = 0;
+            this.pw_textBox.Text = "dgm";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PW:";
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(362, 12);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(79, 72);
+            this.connect_button.TabIndex = 2;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // logs_listBox
+            // 
+            this.logs_listBox.BackColor = System.Drawing.SystemColors.Info;
+            this.logs_listBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logs_listBox.FormattingEnabled = true;
+            this.logs_listBox.HorizontalScrollbar = true;
+            this.logs_listBox.Location = new System.Drawing.Point(18, 100);
+            this.logs_listBox.Name = "logs_listBox";
+            this.logs_listBox.Size = new System.Drawing.Size(422, 524);
+            this.logs_listBox.TabIndex = 3;
+            // 
+            // download_timer
+            // 
+            this.download_timer.Interval = 5000;
+            this.download_timer.Tick += new System.EventHandler(this.download_timer_Tick);
+            // 
+            // ftp_tester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 633);
+            this.Controls.Add(this.logs_listBox);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pw_textBox);
+            this.Controls.Add(this.id_textBox);
+            this.Controls.Add(this.ip_textBox);
+            this.Name = "ftp_tester";
+            this.Text = "FTP Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ftp_tester_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ip_textBox;
+        private System.Windows.Forms.TextBox id_textBox;
+        private System.Windows.Forms.TextBox pw_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.ListBox logs_listBox;
+        private System.Windows.Forms.Timer download_timer;
+    }
+}
+
---
+++ GetDocumentsViaFTP/GetDocumentsViaFTP/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FluentFTP;
+using System.Net;
+
+namespace GetDocumentsViaFTP
+{
+    public partial class ftp_tester : Form
+    {
+        FtpClient client = null;
+
+        public ftp_tester()
+        {
+            InitializeComponent();
+
+            // create an FTP client
+            this.client = new FtpClient();
+        }
+
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+             this.client.Host = this.ip_textBox.Text;
+
+            // if you don't specify login credentials, we use the "anonymous" user account
+            this.client.Credentials = new NetworkCredential(this.id_textBox.Text, this.pw_textBox.Text);
+
+            // begin connecting to the server     
+            this.client.Connect();
+            if (true == this.client.IsConnected)
+            {
+                // Change Encoding
+                this.client.Encoding = Encoding.UTF8;
+                this.MessageHandler_Message(string.Format("Connet to IP: {0}", this.ip_textBox.Text));
+
+                this.download_timer.Interval = 2000;
+                this.download_timer.Start();         
+            }
+        }
+
+        private void MessageHandler_Message(string sMessage)
+        {
+            var writeMessageAction = new Action(
+                () =>
+                {
+                    this.logs_listBox.BeginUpdate();
+
+                    int nItem = this.logs_listBox.Items.Add(string.Format("[{0}] {1}", System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), sMessage));
+
+                    if (this.logs_listBox.Items.Count > 5000)
+                    {
+                        this.logs_listBox.Items.RemoveAt(0);
+                    }
+
+                    if (this.logs_listBox.SelectedIndex < 0)
+                    {
+                        this.logs_listBox.TopIndex = nItem;
+                    }
+                    else if (this.logs_listBox.SelectedIndex == nItem - 1)
+                    {
+                        this.logs_listBox.SelectedIndex = nItem;
+                    }
+
+                    this.logs_listBox.EndUpdate();
+                });
+
+            if (this.logs_listBox.InvokeRequired)
+                this.logs_listBox.Invoke(writeMessageAction);
+            else
+                writeMessageAction();
+        }
+
+        private void download_timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (FtpListItem item in this.client.GetListing(this.client.GetWorkingDirectory()))
+                {
+                    switch (item.Type)
+                    {
+                        case FtpFileSystemObjectType.Directory:
+                            break;
+                        case FtpFileSystemObjectType.File:
+                            this.MessageHandler_Message(string.Format("File Name: {0}", item.FullName));
+                            this.client.DownloadFile(string.Format(@"D:\TCPIP_test\dgme\{0}", item.Name), item.FullName);
+                            this.client.DeleteFile(item.FullName);
+                            this.MessageHandler_Message(string.Format("Finished: {0} (size:{1})", item.FullName, item.Size));
+                            break;
+                        case FtpFileSystemObjectType.Link:
+                            break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+   
+        }
+
+        private void ftp_tester_FormClosing(object sender, FormClosingEventArgs e)
+        {            
+            this.download_timer.Stop();
+            this.client.Disconnect();
+        }
+    }
+}
